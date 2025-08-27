c84aedab75a19d69754426581468b1d3cd96757f
ghaziagerbi <69095280+ghaziagerbi@users.noreply.github.com>
2025-08-27 20:42:34 +0200

    Fix zip extraction using Node.js

---
+++ blogcraft-dash-main/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>BlogCraft Dashboard</h1>
+        <p>Welcome to your blog management dashboard</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
